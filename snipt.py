c916501b917479d2bc1ba156437d9d5da13f25e3
Clasko <clasko@flyingbits.de>
2012-05-10 20:52:40 +0200

    Added Support for private API. some little Bugfixes.

--- snipt.py
+++ snipt.py
@@ -8,6 +8,7 @@ class SyncSniptCommand(sublime_plugin.TextCommand):
         self.username = self.settings.get('snipt_username')
         self.apikey = self.settings.get('snipt_apikey')
         self.userid = self.settings.get('snipt_userid')
+        self.apimode = self.settings.get('snipt_apimode')
 
         if (not self.apikey):
             sublime.error_message('No snipt.net apikey. You must first set you apikey in: Sublime Text2 ~> Preferences ~> Package Settings ~> Snipt Tools ~> Settings')
@@ -21,18 +22,24 @@ class SyncSniptCommand(sublime_plugin.TextCommand):
             sublime.error_message('No snipt.net username. You must first set you username in: Sublime Text2 ~> Preferences ~> Package Settings ~> Snipt Tools ~> Settings')
             return
 
+        if (not self.apimode):
+            sublime.error_message('No snipt.net apimode. You must first set you API mode in: Sublime Text2 ~> Preferences ~> Package Settings ~> Snipt Tools ~> Settings')
+            return
+
     def run(self, edit):
         # check for userinfos config
         self.get_userinfos()
         username = self.username
         apikey = self.apikey
         userid = self.userid
-      
+        apimode = self.apimode
+
         # grab the user data
         try:
-            response = urllib2.urlopen('https://snipt.net/api/public/snipt/?user={0}'.format(userid))
-            #for private API
-            #response = urllib2.urlopen('https://snipt.net/api/public/snipt/?username={0}&api_key={1}&format=json'.format(username, apikey))
+            if (apimode == "public"):
+                response = urllib2.urlopen('https://snipt.net/api/public/snipt/?user={0}'.format(userid))
+            else:
+                response = urllib2.urlopen('https://snipt.net/api/private/snipt/?username={0}&api_key={1}&format=json'.format(username, apikey))
         except urllib2.URLError, (err):
             sublime.error_message("Connection refused. Try again later. Snipt step: 1"+err)
             return
@@ -48,12 +55,14 @@ class SyncSniptCommand(sublime_plugin.TextCommand):
              
             rx = re.compile('\W+')
             cleantitle = rx.sub(' ', title).strip()
+            
+            if (code):
+                # lets turn wine (snipts) into water (sublime snippets)
+                buildfile = sublime.packages_path()+'/Sublime-Snipt/repo/{0}.sublime-snippet'.format(cleantitle[0:20])
+                newfile = open(buildfile,'w+')
+                newfile.write('<snippet><content><![CDATA[{0}]]></content><tabTrigger>snipt</tabTrigger></snippet>'.format(code))
+                newfile.close()
 
-            # lets turn wine (snipts) into water (sublime snippets)
-            buildfile = 'repo/{0}.sublime-snippet'.format(cleantitle[0:20])
-            newfile = open(buildfile,'w+')
-            newfile.write('<snippet><content><![CDATA[{0}]]></content><tabTrigger>snipt</tabTrigger></snippet>'.format(code))
-            newfile.close()
 
 # This is coming soon. Ability to send text to snipt.net and create a sublime snippet.
 # class CreateSniptCommand(sublime_plugin.TextCommand):
